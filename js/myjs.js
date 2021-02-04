e6180b0356a01a85b6992f552009f44683ef1824
Evgen Bielich <belichevgenij@gmail.com>
2021-02-04 23:43:15 +0200

    модуль 1

---
+++ js/myjs.js
@@ -0,0 +1,86 @@
+// Возведение числа в степень
+
+// let base = prompt('Давай число');
+// base = Number(base);
+
+// console.log(base)
+
+// let power = prompt("Давай степень");
+// power = Number(power);
+
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+// Смена цвета фона Боди
+
+// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
+
+
+// Приветственное сообщение 
+
+// const room = 584;
+// const type = 'VIP';
+// const firstName = "John";
+// const lastNAme = "Black";
+
+// const welcomMsg = `Гость ${firstName} ${lastNAme} поселяется в ${type} номер ${room}`;
+// console.log(welcomMsg);
+
+
+// Нормализация
+
+// let brand = prompt('ДАвай бренд:');
+// brand = brand.toLowerCase();
+// console.log(brand);
+
+
+// Логические операторы
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 20;
+
+// console.log(`Число ${number} попадает в отрезок до ${x1}?`, number < x1);
+// console.log(`Число ${number} попадает в отрезок после ${x2}?`, number > x2);
+// console.log(`ЧИсло ${number} попадает в отрезок от ${x1} до ${x2}`, number > x1 && number < x2);
+// console.log(`ЧИсло ${number} попадает в отрезок до ${x1} или после ${x2}`, number < x1 || number > x2);
+
+
+
+// разрешает открыть чат - если онлайн, друг, и режим"не беспокоить""
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log(`Можно открыть чат?`, canOpenChat);
+
+
+
+// подписка, если пользователь про или вип тогда есть доступ
+
+// const sub = 'free';
+// const canAccessContent = sub === 'pro' || sub === 'vip';
+
+
+
+// тернарный оператор
+
+// const balance = 1000;
+// const message = balance >= 0 ? `Позитивный баланс` : `Негативный баланс`;
+// console.log(message);
+
+let balance = 1000;
+const payment = 2000;
